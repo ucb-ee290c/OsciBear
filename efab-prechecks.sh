c6380f9f55309aa96d5fa851361b995a45fdde9f
Dan Fritchman <dan@fritch.mn>
2021-06-23 12:29:55 -0700

    Add eFab pre-check script to CI

---
+++ efab-prechecks.sh
@@ -0,0 +1,12 @@
+
+# Run eFabless's pre-checks 
+set -e
+
+make install 
+make precheck 
+make run-precheck \
+	CARAVEL_ROOT=${PWD}/caravel \
+	PRECHECK_ROOT=${PWD}/open_mpw_precheck \
+	PDK_ROOT=/tools/B/dan_fritchman/sky130/local/sky130A
+
+
